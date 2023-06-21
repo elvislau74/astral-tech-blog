544cc96d3d172c2c30d69fe1cdc3fbcb1150848c
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-06-21 17:58:02 -0400

    create seed.js file to seed the tables and add rows based on the models, edit the json files to have correct keys

---
+++ seeds/seed.js
@@ -0,0 +1,36 @@
+const sequelize = require('../config/connection');
+const { User, Blogpost, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogpostData = require('./blogpostData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blogpost of blogpostData) {
+    await Blogpost.create({
+      ...blogpost,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  const createdBlogposts = await Blogpost.findAll();
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      blogpost_id: createdBlogposts[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
