6e5d2a8fc0c57d6543d7b54ab91120767123a858
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-06-21 20:34:37 -0400

    add index.js file and homeRoutes.js file in controllers folder to link to homepage, create and start writing code in homepage.handlebars file in views folder

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { Blogpost, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const blogpostData = await Blogpost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['message']
+                },
+            ],
+        });
+
+        const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+
+        res.render('homepage', {
+            blogposts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
